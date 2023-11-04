cd99d544298eb8ed3662ea5b3a353322a30fd0ff
root <you@example.com>
2023-11-04 02:31:28 +0000

    all endpoints listen to all methods now

--- main.go
+++ main.go
@@ -64,53 +64,53 @@ func main() {
 	r.Use(corsMiddleware.Handler)
 
 	// Ping endpoint
-	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("pong"))
 	})
 
-	r.Get("/to/301", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/to/301", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("301"))
 	})
 
-	r.Get("/to/302", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/to/302", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("302"))
 	})
 
-	r.Get("/status/200", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/200", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
-	r.Get("/status/201", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/201", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte("Created"))
 	})
 
-	r.Get("/status/204", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/204", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNoContent)
 		// No content to return
 	})
 
-	r.Get("/status/301", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/301", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/to/301", http.StatusMovedPermanently)
 	})
 
-	r.Get("/status/302", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/302", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/to/302", http.StatusFound)
 	})
 
 	//304 Not Modified
-	r.Get("/status/304", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/304", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotModified)
 		// No content to return
 	})
 
-	r.Get("/status/400", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/400", func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 	})
 
 	// Random JSON endpoint
-	r.Get("/json/random", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/json/random", func(w http.ResponseWriter, r *http.Request) {
 		// Generate random values of different types
 		rand.Seed(time.Now().UnixNano())
 		response := map[string]interface{}{
@@ -126,38 +126,38 @@ func main() {
 		json.NewEncoder(w).Encode(response)
 	})
 
-	r.Get("/status/401", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/401", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("WWW-Authenticate", `Basic realm="restricted area"`)
 		http.Error(w, "401 Unauthorized", http.StatusUnauthorized)
 	})
 
-	r.Get("/status/403", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/403", func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Forbidden", http.StatusForbidden)
 	})
 
 	// 404 Not found
-	r.Get("/status/404", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/404", func(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 	})
 
-	r.Get("/status/408", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/408", func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Request Timeout", http.StatusRequestTimeout)
 	})
 
-	r.Get("/status/500", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/500", func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	})
 
-	r.Get("/status/501", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/501", func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Not Implemented", http.StatusNotImplemented)
 	})
 
-	r.Get("/status/503", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/status/503", func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
 	})
 
 	// Cookie random endpoint
-	r.Get("/cookie/random", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/cookie/random", func(w http.ResponseWriter, r *http.Request) {
 		// Generate a random number as the cookie value
 		rand.Seed(time.Now().UnixNano())
 		randomValue := fmt.Sprintf("%d", rand.Intn(1000000))
@@ -174,7 +174,7 @@ func main() {
 	})
 
 	// Cookie random endpoint for setting a specified number of cookies
-	r.Get("/cookie/random/{number}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/cookie/random/{number}", func(w http.ResponseWriter, r *http.Request) {
 		// Get the number from the URL parameter
 		numberStr := chi.URLParam(r, "number")
 		number, err := strconv.Atoi(numberStr)
